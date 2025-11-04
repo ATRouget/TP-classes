08af62805210bcf9193a4f5ef811d4d40768830d
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2025-11-04 16:22:47 +0100

    try get student

--- TP_classes_classes.py
+++ TP_classes_classes.py
@@ -7,7 +7,7 @@ class Class:
         self.pupils = {}
 
     def add_student(self, student: Student):
-        self.pupils[student] = 1
+        self.pupils[student.first_name + " " + student.last_name] = student
 
     def __len__(self):
         return len(list(self.pupils.keys()))
@@ -15,15 +15,24 @@ class Class:
     def __repr__(self):
         return "Class " + self.classname + " - " + f"{len(self)}" + " student(s)"
 
+    def get_student(self, first_name: str, last_name:str):
+        if first_name + " " + last_name in self.pupils :
+            return self.pupils[first_name + " " + last_name]
+        else :
+            return None
+
+
+
+
+
 try:
     classe = Class("P20")
     student = Student("Matthieu", "Mazière")
     classe.add_student(student)
-    print (repr(classe))
-    if len(classe) != 1:
-        raise Exception('OOPS - There is an issue in your __len__ method.')
-    if repr(classe) != "Class P20 - 1 student(s)":
-        raise Exception('OOPS - There is an issue in your __repr__ method.')
+    new_student = classe.get_student("Matthieu", "Mazière")
+    assert student == new_student
+    new_student = classe.get_student("Jérôme", "Adnot")
+    assert new_student is None
 except Exception as e:
     print("OOPS - Something's wrong")
     print(f"Error message : {e}")
